964a31a2bb408b54691e383333cf2e6e7f57500c
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 01:19:17 +0500

    This is 45 Question Assignment Q-27.
    This is 45 Question Assignment Q-27.

---
+++ app.ts
@@ -0,0 +1,11 @@
+let alienColor: string = "green";
+
+if (alienColor === "green") {
+    console.log("You just earned 5 points for shooting the green alien.");
+} else if (alienColor === "yellow") {
+    console.log("You just earned 10 points for shooting the yellow alien.");
+} else if (alienColor === "red") {
+    console.log("You just earned 15 points for shooting the red alien.");
+} else {
+    console.log("Unknown alien color. No points earned.");
+}
